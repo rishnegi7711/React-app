c45b5d0cc3363d329bfc87e0d8bf747e22c43a81
rishnegi7711 <rishnegi28@gmail.com>
2021-11-22 15:13:30 +0530

    Added username text field and validations to all fields

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,10 +8,59 @@ import './Login.scss'
 const Login = () => {
 
     const [isUsernameError, setIsUsernameError] = useState(false)
-    const [isPasswordError,setIsPasswordError]= useState(false);
-    const [enteredPassword1,setEnteredPassword1] = useState('');
-    const [enteredPassword2,setEnteredPassword2] = useState('');
-    const [enteredUsername,setEnteredUsername]=useState('');
+    const [isPassword1Error, setIsPassword1Error] = useState(false);
+    const [isPassword2Error, setIsPassword2Error] = useState(false);
+    const [isEmailError, setIsEmailError] = useState(false);
+    const [enteredPassword1, setEnteredPassword1] = useState('');
+    const [enteredPassword2, setEnteredPassword2] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredUsername, setEnteredUsername] = useState('');
+
+    const usernameChangehandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const password1ChangeHandler = (event) => {
+        setEnteredPassword1(event.target.value)
+    }
+
+    const password2ChangeHandler = (event) => {
+        setEnteredPassword2(event.target.value)
+    }
+    
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value)
+    }
+    
+    const validateEmailHandler = () => {
+
+        if(enteredEmail.trim().length==0 || !enteredEmail.includes('@')){
+            setIsEmailError(true);
+        }
+    }
+
+    const validateUsernameHandler = () => {
+        if(enteredUsername.trim().length==0){
+            setIsUsernameError(true)
+        }
+    }
+
+    const validatePasswordMatchHandler = () => {
+        if(enteredPassword1 != enteredPassword2){
+            setIsPassword2Error(true);
+        }
+    }
+    
+    const validatePassword1Handler= () => {
+        if(enteredPassword1.trim().length==0){
+            setIsPassword1Error(true);
+        }
+    }
+    const validatePassword2Handler= () => {
+        if(enteredPassword2.trim().length==0){
+            setIsPassword2Error(true);
+        }
+    }
 
 
     return <Card className='login'>
@@ -22,22 +71,43 @@ const Login = () => {
             label="Username"
             value={enteredUsername}
             helperText={isUsernameError ? "Incorrect entry." : "Please enter username"}
+            className='textField'
+            onChange={usernameChangehandler}
+            onBlur={validateUsernameHandler}
         />
         <TextField
-            error={isPasswordError}
+            error={isEmailError}
+            id="outlined-error-helper-text"
+            label="Email"
+            value={enteredEmail}
+            helperText={isEmailError ? "Incorrect entry." : "Please enter E-mail"}
+            className='textField'
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+        />
+        <TextField
+            error={isPassword1Error}
             id="outlined-error-helper-text"
             label="Password"
             value={enteredPassword1}
-            helperText={isPasswordError ? "Incorrect entry." : "Please enter password"}
+            helperText={isPassword1Error ? "Incorrect entry." : "Please enter password"}
             type="password"
+            className='textField'
+            onChange={password1ChangeHandler}
+            onBlur={validatePassword1Handler}
+
         />
         <TextField
-            error={isPasswordError}
+            error={isPassword2Error}
             id="outlined-error-helper-text"
             label=" Confirm Password"
             value={enteredPassword2}
-            helperText={isPasswordError ? "Incorrect entry." : "Please enter password again"}
+            helperText={isPassword2Error ? "Incorrect entry." : "Please enter password again"}
             type="password"
+            className='textField'
+            onChange={password2ChangeHandler}
+            onBlur={validatePasswordMatchHandler}
+
         />
         <Button variant="contained" className='button'>Submit</Button>
 
@@ -46,6 +116,6 @@ const Login = () => {
 
 
 
-} 
+}
 
 export default Login
