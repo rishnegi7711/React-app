ed856247a23bd63347032a37309d0fca92016c25
Karan Nehra <karanehra@gmail.com>
2021-11-22 22:33:40 +0530

    Adds files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from '@material-ui/core';
+import Login from './Components/Login';
+
+
+function App(){
+
+return(
+    <div>
+       <Login/> 
+    </div>
+);
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3300,
+        hot:true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins:[new webpack.HotModuleReplacementPlugin()]
+};
