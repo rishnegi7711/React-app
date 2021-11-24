41af0b1e76e6de5f53853248760f478028de97f0
rishnegi7711 <rishnegi28@gmail.com>
2021-11-24 23:31:31 +0530

    Login component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react'
-import {Button} from '@material-ui/core';
+import React, { useState } from 'react'
 import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Switch from '@mui/material/Switch';
 
 
-function App(){
+const App = () => {
 
-return(
-    <div>
-       <Login/> 
-    </div>
-);
+
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
+    return (<React.Fragment>
+        {setChecked && <Signup />}
+        {!setChecked && <Login />}
+        <Switch
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ 'aria-label': 'controlled' }}
+        />
+
+    </React.Fragment>
+
+    )
+
 }
 
 export default App
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,129 +1,68 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import Card from './Card.js'
-import './Login.scss'
+import Card from './Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './Login.scss';
 
 const Login = () => {
-
-    const [isUsernameError, setIsUsernameError] = useState(false)
-    const [isPassword1Error, setIsPassword1Error] = useState(false);
-    const [isPassword2Error, setIsPassword2Error] = useState(false);
-    const [isEmailError, setIsEmailError] = useState(false);
-    const [enteredPassword1, setEnteredPassword1] = useState('');
-    const [enteredPassword2, setEnteredPassword2] = useState('');
-    const [enteredEmail, setEnteredEmail] = useState('');
+    const [isUsernameError, setIsUsernameError] = useState(false);
+    const [isPasswordError, setIsPasswordError] = useState(false);
     const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
 
-    const usernameChangehandler = (event) => {
+    const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
-    }
+    };
 
-    const password1ChangeHandler = (event) => {
-        setEnteredPassword1(event.target.value)
-    }
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    };
 
-    const password2ChangeHandler = (event) => {
-        setEnteredPassword2(event.target.value)
-    }
-    
-    const emailChangeHandler = (event) => {
-        setEnteredEmail(event.target.value)
-    }
-    
-    const validateEmailHandler = () => {
-
-        if(enteredEmail.trim().length==0 || !enteredEmail.includes('@')){
-            setIsEmailError(true);
+    const validatePasswordHandler = () => {
+        if (enteredPassword.trim().length === 0) {
+            setIsPasswordError(true);
         } else {
-            setIsEmailError(false);
+            setIsPasswordError(false)
         }
     }
 
     const validateUsernameHandler = () => {
-        if(enteredUsername.trim().length==0 || enteredUsername.trim().length<6){
+        if (enteredUsername.trim().length === 0 || enteredUsername.trim().length < 6) {
             setIsUsernameError(true)
         } else {
-            setIsUsernameError(false);
+            setIsUsernameError(false)
         }
     }
 
-    const validatePasswordMatchHandler = () => {
-        if(enteredPassword1 != enteredPassword2){
-            setIsPassword2Error(true);
-        } else {
-            setIsPassword2Error(false)
-        }
-    }
-    
-    const validatePassword1Handler= () => {
-        if(enteredPassword1.trim().length==0){
-            setIsPassword1Error(true);
-        } else {
-            setIsPassword1Error(false)
-        }
-    }
-    const validatePassword2Handler= () => {
-        if(enteredPassword2.trim().length==0){
-            setIsPassword2Error(true);
-        }
-    }
+    return (
+        <Card className="signup">
+            <h1>Login</h1>
+            <TextField
+                error
+                id="outlined-error-helper-text"
+                label="Username"
+                defaultValue="Username"
+                value={enteredUsername}
+                helperText={isUsernameError ? 'Incorrect Username.' : 'Please enter Username'}
+                onChange={usernameChangeHandler}
+                onBlur={validateUsernameHandler}
+            />
+            <TextField
+                error
+                id="outlined-error-helper-text"
+                label="Password"
+                defaultValue="Password"
+                value={enteredPassword}
+                helperText={isPasswordError ? 'Incorrect password.' : 'Please enter Password'}
+                type="password"
+                onChange={passwordChangeHandler}
+                onBlur={validatePasswordHandler}
+            />
+            <Button variant="contained" className="button" size="medium">
+                Log In
+            </Button>
+        </Card>
+    );
+};
 
-
-    return <Card className='login'>
-        <h1>Sign Up</h1>
-        <TextField
-            error={isUsernameError}
-            id="outlined-error-helper-text"
-            label="Username"
-            value={enteredUsername}
-            helperText={isUsernameError ? "Incorrect entry." : "Please enter username"}
-            className='textField'
-            onChange={usernameChangehandler}
-            onBlur={validateUsernameHandler}
-        />
-        <TextField
-            error={isEmailError}
-            id="outlined-error-helper-text"
-            label="Email"
-            value={enteredEmail}
-            helperText={isEmailError ? "Incorrect entry." : "Please enter E-mail"}
-            className='textField'
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-        />
-        <TextField
-            error={isPassword1Error}
-            id="outlined-error-helper-text"
-            label="Password"
-            value={enteredPassword1}
-            helperText={isPassword1Error ? "Incorrect entry." : "Please enter password"}
-            type="password"
-            className='textField'
-            onChange={password1ChangeHandler}
-            onBlur={validatePassword1Handler}
-
-        />
-        <TextField
-            error={isPassword2Error}
-            id="outlined-error-helper-text"
-            label=" Confirm Password"
-            value={enteredPassword2}
-            helperText={isPassword2Error ? "Incorrect entry." : "Please enter password again"}
-            type="password"
-            className='textField'
-            onChange={password2ChangeHandler}
-            onBlur={validatePasswordMatchHandler}
-
-        />
-        <Button variant="contained" className="button" size="medium">Submit</Button>
-
-
-    </Card>
-
-
-
-}
-
-export default Login
+export default Login;
