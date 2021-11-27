797e6f32cef875c7973edee4a4eece2f9290e9d9
rishnegi7711 <rishnegi28@gmail.com>
2021-11-27 17:31:19 +0530

    Adjusted email validator logic to show error

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -36,9 +36,9 @@ const Signup = () => {
 
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (re.test(String(enteredEmail).trim())) {
-            setIsEmailError(true);
-        } else {
             setIsEmailError(false);
+        } else {
+            setIsEmailError(true);
         }
 
     }
