0c6d935d75188a77f186c4dac7f8109e382b4401
rishnegi7711 <rishnegi28@gmail.com>
2021-12-20 18:42:56 +0530

    Minor fix to routes

--- src/Components/RoutePaths.js
+++ src/Components/RoutePaths.js
@@ -3,19 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 
 const RoutePaths = () => {
-
     return (
         <Routes>
             <Route path="*" element={<main><h2>There is nothing here!</h2></main>} />
-            <Route path="/" element={BasePage} />
-            <Route path="/login" element={BasePage} />
-            <Route path="/signup" element={BasePage} />
-
+            <Route path="/" element={<BasePage />} />
+            <Route path="/login" element={<BasePage />} />
+            <Route path="/signup" element={<BasePage />} />
         </Routes>
-
-
     )
-
 }
 
 export default RoutePaths
