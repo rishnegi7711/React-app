45549b8e6c1901e1a5de0fffe45f2c0ba74eb4dd
rishnegi7711 <rishnegi28@gmail.com>
2021-12-22 09:34:30 +0530

    Layout change for switch on basepage

--- src/Components/BasePage.js
+++ src/Components/BasePage.js
@@ -2,24 +2,34 @@ import React from "react";
 import Switch from '@mui/material/Switch';
 import Signup from "./Signup";
 import Login from "./Login";
+import Card from "./Card"
+import "./BasePage.scss";
 import { useState } from "react";
+
+
+
+
 
 
 const BasePage = () => {
 
-    const [checked, setChecked] = useState(false);
+
+
     const [isLoginPageActive, setIsLoginPageActive] = useState(false);
 
     return (
-        <>
-            {checked ? <Signup /> : <Login />}
-            <h3>Login</h3>
-            <Switch
-                checked={checked}
-                onChange={e => setChecked(e.target.checked)}
-            />
-            <h3>Signup</h3>
-        </>
+        <Card className='basePage'>
+            {isLoginPageActive ? <Signup /> : <Login />}
+            <div className='switch'>
+                <h3>Login</h3>
+                <Switch
+                    checked={isLoginPageActive}
+                    onChange={e => setIsLoginPageActive(e.target.checked)}
+                />
+                <h3>Signup</h3>
+            </div>
+
+        </Card>
     )
 
 }
